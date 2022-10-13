813740c9600fb2b8cd3fca36f502af430fe22550
TalentedNoble <ahmedridwan0822@gmail.com>
2022-10-13 13:44:32 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  *main- Serves as the entry point for the program
  *
- *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *Return: By default returns zero if no erros otherwise returns non-\
+zero value.
 */
 int main(void)
 {
